27f3ad5ee14b053691f3eb462510b039fdbadb45
feniljain <fkjainco@gmail.com>
2024-09-18 15:50:48 +0530

    feat: create(using spark) and read hudi tables

---
+++ src/hudi_manager.rs
@@ -0,0 +1,31 @@
+// use std::sync::Arc;
+
+use hudi::table::Table;
+
+pub struct HudiManager {}
+
+impl HudiManager {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub async fn read_table(&self, path: &str) -> anyhow::Result<()> {
+        // let ctx = SessionContext::new();
+        // let hudi = HudiDataSource::new(path).await?;
+        //
+        // ctx.register_table("tbl", Arc::new(hudi))?;
+        //
+        // let df: DataFrame = ctx.sql("SELECT * from tbl").await?;
+        // df.show().await?;
+        //
+        //
+
+        let tbl = Table::new(path).await?;
+        let data = tbl.read_snapshot().await?;
+
+        println!("data: {data:?}");
+
+        Ok(())
+    }
+}
+
